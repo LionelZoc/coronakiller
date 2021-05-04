dcf6070ba90f83bdddfb272ca5079ef28484300f
lionel zoc <lionel@wisembly.com>
2021-05-04 09:30:00 +0200

    remove ref at start

--- App.js
+++ App.js
@@ -96,12 +96,12 @@ const App = () => {
     //don't update if component is unmounted
     //console.log("in handlefinishloading with current", isMounted.current);
     Sentry.Native.captureMessage("finish loading");
-    if (isMounted.current === true) {
-      Sentry.Native.captureMessage(
-        "finish loading and will set loading complete to true"
-      );
-      setIfLoadingComplete(true);
-    }
+    //if (isMounted.current === true) {
+    Sentry.Native.captureMessage(
+      "finish loading and will set loading complete to true"
+    );
+    setIfLoadingComplete(true);
+    //}
   };
 
   if (!ifLoadingComplete) {
