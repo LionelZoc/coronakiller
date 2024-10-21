08c5be8f323abe02eeae4443c61fedad11ca8612
lionel zoc <zoc.lionel@gmail.com>
2024-10-21 23:25:08 +0200

    TASK-01:  from 41 to 51 / migration to expo router
    update some packages

---
+++ src/app/(drawer)/board.tsx
@@ -0,0 +1,5 @@
+import Board from "components/Board";
+
+export default function BoardScreen() {
+  return <Board />;
+}
---
+++ src/app/(drawer)/game-presentation.tsx
@@ -0,0 +1,5 @@
+import GamePresentation from "components/GamePresentation";
+
+export default function GamePresentationScreen() {
+  return <GamePresentation />;
+}
